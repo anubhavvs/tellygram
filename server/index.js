56df16d15caa6ef59bb132a05bab2fea6f6a57f7
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-07-22 21:40:29 +0530

    apollo server setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const { ApolloServer } = require('apollo-server');
 const connectDB = require('./database/db');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 require('dotenv').config();
 
 connectDB();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: (context) => context
+});
+
+server.listen({port: process.env.PORT}).then(({url, subscriptionsUrl}) => {
+    console.log(`Server running on ${url}`);
+    console.log(`Subscriptions running on ${subscriptionsUrl}`);
+})
 
 console.log(process.env.PORT);
