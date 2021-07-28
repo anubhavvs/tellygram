a3ed2446e1beccd37731d750b491e16436f5e997
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-07-28 21:42:10 +0530

    apollo client setup

---
+++ client/src/utils/localStorage.js
@@ -0,0 +1,20 @@
+const storageToken = 'tellygramUserKey'
+
+const saveUser = (user) => {
+    localStorage.setItem(storageToken, JSON.stringify(user));
+}
+
+const loadUser = () => {
+    JSON.parse(localStorage.getItem(storageToken));
+}
+
+const removeUser = () => {
+    localStorage.removeItem(storageToken);
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    saveUser,
+    loadUser,
+    removeUser
+}
