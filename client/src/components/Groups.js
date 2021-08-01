812fd97ad9681552b537c22634e8fcfb496653d9
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-08-01 22:51:59 +0530

    sidebar and logout functionality

---
+++ client/src/components/Groups.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Groups = () => {
+    return (
+        <div>
+            Groups page
+        </div>
+    )
+}
+
+export default Groups;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Chats from '../components/Chats';
+import Groups from '../components/Groups';
+import Sidebar from '../components/Sidebar';
+import Users from '../components/Users';
 
 const Home = () => {
+    const [tab, setTab] = useState('chat');
+    
     return (
-        <div>
-            Home Page
+        <div className="flex flex-row min-h-screen bg-gray-50">
+            <Sidebar changeTab={(value) => setTab(value)} />
+            { tab === 'chat' ? (
+                <Chats />
+            ) : tab === 'users' ? (
+                <Users />
+            ) : (
+                <Groups />
+            )}
         </div>
     )
 }
