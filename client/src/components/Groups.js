e4905a43b90873b15ad8b8e3d746338582f027e8
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-08-02 15:45:00 +0530

    group list view

--- client/src/components/Groups.js
+++ client/src/components/Groups.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_GROUPS } from '../graphql/queries';
+import { useStateContext } from '../context/state';
+import { getErrorMessage } from '../utils/helperFunctions';
+import Loader from './Loader';
+import Search from './Search';
 
 const Groups = () => {
+    const { selectedChat, selectChat, notify } = useStateContext();
+    const [searchText, setSearchText] = useState('');
+    const { data: groupData, loading: loadingGroups } = useQuery(GET_GROUPS, {
+        onError: (err) => {
+            notify(getErrorMessage(err), 'error');
+        }
+    });
+
+    if(loadingGroups) {
+        return <Loader />
+    }
+
     return (
-        <div>
-            Groups page
+        <div className="flex-col items-center justify-center flex-none w-1/4 bg-gray-300 p-8">
+            <div className="flex flex-row justify-between items-center mb-8">
+                <p className="font-semibold text-2xl">Your Groups</p>
+                <button className="flex text-md opacity-80">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                    </svg>
+                    Create Group
+                </button>
+            </div>
+            <Search placeholderText='Search Groups...' searchText={searchText} onChange={value => setSearchText(value)}/>
+            {groupData && groupData.getGroups.length === 0 && (
+                <p className="text-xl rounded-xl bg-gray-400 px-2 py-5">You are not part of any group!☹️</p>
+            )}
+            {groupData && groupData.getGroups.filter((group) =>group.name.toLowerCase().includes(searchText.toLocaleLowerCase())).map((group) => (
+                <div key={group.id} className={`h-20 text-white font-semibold flex space-x-7 py-2 px-3 mb-3 rounded-xl cursor-pointer hover:bg-gray-500 ${selectedChat?.chatType === 'group' && group.id === selectedChat.chatData.id ? `bg-gray-500` : `bg-gray-700`}`} onClick={() => selectChat(group, 'group')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 my-auto rounded-full bg-white" fill="none" viewBox="0 -6 24 40" stroke="black">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    <p className="my-auto text-sm">{group.name}</p>
+                </div>
+            ))}
         </div>
     )
 }
