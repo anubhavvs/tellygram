e8266b32409a9ae5e2acb233ed5621052f3fca03
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-08-02 18:33:33 +0530

    recent date formatter

--- client/src/utils/helperFunctions.js
+++ client/src/utils/helperFunctions.js
@@ -1,3 +1,5 @@
+import { isToday, isYesterday, format, differenceInCalendarDays } from 'date-fns';
+
 export const getErrorMessage = (err) => {
     if(err.graphQLErrors[0]?.message) {
         return err.graphQLErrors[0].message;
@@ -11,3 +13,19 @@ export const truncateString = (string, maxCharLimit) => {
       ? string
       : string.slice(0, maxCharLimit) + '...';
 };
+
+const isSameWeek = (date) => {
+    const result = differenceInCalendarDays(new Date(), new Date(date)) <= 6 ? true : false;
+    return result;
+}
+
+export const formatRecentDate = (date) => {
+    const day = isToday(new Date(date))
+        ? format(new Date(date), 'h:mm a')
+        : isYesterday(new Date(date))
+        ? 'Yesterday'
+        : isSameWeek(new Date(date))
+        ? format(new Date(date), 'EEEE')
+        : format(new Date(date), 'dd/MM/yy')
+    return day;
+} 
