e844b9e6f0d8caec5b8e79a1dbe2e0fb8cc93786
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-08-03 02:02:43 +0530

    search box fix

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -6,7 +6,7 @@ const Search = ({ placeholderText, searchText, onChange }) => {
             <span className="flex items-center">
                 <svg fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </span>
-            <input value={searchText} onChange={ event => onChange(event.target.value)} className="bg-gray-700 ml-5 rounded-md focus:outline-none text-white" placeholder={placeholderText} autoComplete="off" />
+            <input value={searchText} onChange={ event => onChange(event.target.value)} className="w-full bg-gray-700 ml-5 rounded-md focus:outline-none text-white" placeholder={placeholderText} autoComplete="off" />
         </div>
     )
 }
