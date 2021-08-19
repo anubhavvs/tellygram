a1e72804b24609f5621bbbe3ca1c31810d80f4e1
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-08-20 05:07:40 +0530

    message bubble

---
+++ client/src/components/Bubble.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useAuthContext } from '../context/auth';
+import { formatBubbleTime } from '../utils/helperFunctions';
+
+const Bubble = ({ message }) => {
+    const { user } = useAuthContext();
+    const isSentMessage = message.senderId === user.id;
+    return (
+        <div className={`bg-indigo-900 mx-2 mb-2 py-2 px-3 rounded-xl shadow-lg text-white ${isSentMessage ? `justify-items-end` : `text-left`} w-max flex flex-row`}>
+            <p className="text-lg">{message.body}</p>
+            <p className="text-xs mt-3 ml-2">{formatBubbleTime(message.createdAt)}</p>
+        </div>
+    )
+}
+
+export default Bubble;
