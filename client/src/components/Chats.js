d442d47a6fc465a962dfe22ef429b8a2351a82b0
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-08-20 05:09:30 +0530

    chat list fix

--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -36,7 +36,7 @@ const Chats = () => {
             </div>
             <Scrollbars hideTracksWhenNotNeeded autoHide>
             {globalData && (
-                <div className={`h-20 text-white font-semibold flex space-x-5 py-2 px-3 mb-3 rounded-xl cursor-pointer hover:bg-gray-500 ${selectedChat?.chatType === 'public' && globalData.getGlobalGroup.id === selectedChat.chatData.id ? `bg-gray-500` : `bg-gray-700`}`} onClick={() => selectChat(globalData.getGlobalGroup, 'group')}>
+                <div className={`h-20 text-white font-semibold flex space-x-5 py-2 px-3 mb-3 rounded-xl cursor-pointer hover:bg-gray-500 ${selectedChat?.chatType === 'public' && globalData.getGlobalGroup.id === selectedChat.chatData.id ? `bg-gray-500` : `bg-gray-700`}`} onClick={() => selectChat(globalData.getGlobalGroup, 'public')}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-14 w-14 my-auto bg-white rounded-full" fill="none" viewBox="0 -3 24 30" stroke="black">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                     </svg>
