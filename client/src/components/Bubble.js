eb7f15c46f847e8f4edea1d477e7c4ccbbcd601f
Anubhav Saha <anubhavvsaha2001@gmail.com>
2021-08-21 13:32:23 +0530

    chat bubble fix

--- client/src/components/Bubble.js
+++ client/src/components/Bubble.js
@@ -6,7 +6,7 @@ const Bubble = ({ message }) => {
     const { user } = useAuthContext();
     const isSentMessage = message.senderId === user.id;
     return (
-        <div className={`bg-indigo-900 mx-2 mb-2 py-2 px-3 rounded-xl shadow-lg text-white ${isSentMessage ? `justify-items-end` : `text-left`} w-max flex flex-row`}>
+        <div className={`bg-indigo-900 mx-2 mb-2 py-2 px-3 rounded-xl shadow-lg text-white ${isSentMessage ? `justify-items-end` : `text-left`} w-1/2 max-w-max flex flex-row`}>
             <p className="text-lg">{message.body}</p>
             <p className="text-xs mt-3 ml-2">{formatBubbleTime(message.createdAt)}</p>
         </div>
