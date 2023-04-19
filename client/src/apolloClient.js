df5eb00414b1046d8030cfd83bbcb835604a3a85
Anubhav Saha <anubhavvsaha2001@gmail.com>
2023-04-19 21:11:23 +0200

    server update

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,66 +1,67 @@
-import { ApolloClient, HttpLink, InMemoryCache, split} from '@apollo/client';
-import { setContext } from 'apollo-link-context';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
-import storage from './utils/localStorage';
+import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client";
+import { setContext } from "apollo-link-context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+import storage from "./utils/localStorage";
 
-const http = 'https://tellygram-backend.herokuapp.com'
-const ws = 'wss://tellygram-backend.herokuapp.com/graphql'
+const http = "https://t-b7ld.onrender.com/";
+const ws = "wss://t-b7ld.onrender.com/graphql";
 
 const authLink = setContext((_, { headers }) => {
-    const loggedUser = storage.loadUser();
+  const loggedUser = storage.loadUser();
 
-    return {
-        headers: {
-            ...headers,
-            authorization: loggedUser ? loggedUser.token : null
-        }
-    };
+  return {
+    headers: {
+      ...headers,
+      authorization: loggedUser ? loggedUser.token : null,
+    },
+  };
 });
 
 const httpLink = new HttpLink({
-    uri: http
+  uri: http,
 });
 
 const wsLink = new WebSocketLink({
-    uri: ws,
-    options: {
-        lazy: true,
-        reconnect: true,
-        connectionParams: () => {
-            return {
-                Authorization: storage.loadUser()?.token
-            };
-        }
-    }
+  uri: ws,
+  options: {
+    lazy: true,
+    reconnect: true,
+    connectionParams: () => {
+      return {
+        Authorization: storage.loadUser()?.token,
+      };
+    },
+  },
 });
 
 const splitLink = split(
-    ({ query }) => {
-        const defination = getMainDefinition(query);
-        return (
-            defination.kind === 'OperationDefinition' && defination.operation === 'subscription'
-        );
-    },
-    wsLink,
-    authLink.concat(httpLink)
+  ({ query }) => {
+    const defination = getMainDefinition(query);
+    return (
+      defination.kind === "OperationDefinition" &&
+      defination.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
 );
 
 const client = new ApolloClient({
-    cache: new InMemoryCache({
-        typePolicies: {
-            Query: {
-                fields: {
-                    getGroups: {
-                        merge(existing, incoming) {
-                            return incoming;
-                        }
-                    }
-                }
-            }
-        }
-    }),
-    link: splitLink
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          getGroups: {
+            merge(existing, incoming) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
+  link: splitLink,
 });
 
 export default client;
